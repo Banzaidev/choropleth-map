25f89c4451cef0bcbb493f7a62a6008d16a950b1
= <banzaideveloper@gmail.com>
2025-02-27 12:31:22 +0100

    Add geoJSON data, and draw US map

---
+++ src/main.js
@@ -0,0 +1,49 @@
+import './style.css'
+import * as d3 from "d3";
+import { feature } from 'topojson-client';
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1 id='title'>United States Educational Attainment</h1>
+    <h4 id='description'>Percentage of adults age 25 and older with a bachelor's degree or higher (2010-2014)</h4>
+    <div id='graph'></div>
+  </div>
+`
+
+const graphSize = {
+  width: 1000,
+  height: 1000
+}
+
+d3.select('#graph')
+.append('svg')
+.attr('width',graphSize.width)
+.attr('height',graphSize.height)
+
+//const projection = d3.geoAlbersUsa() Serve per creare una proiezione geografica predefinita degli stati uniti, e da solo
+// può convertitre latitudine e longitudine in pixel 
+const path = d3.geoPath()
+/* d3.geoPath() serve a generare  percorsi svg, sfruttando i dati GeoJSON (coordinate lat e long)
+sfruttando d3.geoPath().projection(proiezione d3.geoAlbersUsa()) */
+/* Non devo applicare un ulteriore proiezione perché i dati nel file JSON sono pre proiettati */
+
+
+const dataCountyJSON = await d3.json('https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json').then(data => data)
+const geoJSON = feature(dataCountyJSON, dataCountyJSON.objects.counties).features
+console.log(feature(dataCountyJSON, dataCountyJSON.objects.counties))
+//feature(dati da d3.json, oggetto specifico che contiene i dati) restituisce un oggetto e i dati geoJSON ottenuti dal topoJSON sono contenuti in features
+/* I dati nel file JSON sono espressi sottoforma di TopoJSON questo vuol dire che sono espressi sottoforma di archi condivisi 
+I dati in un geoJSON esprimono la geometria con punti, linee e poligoni disposti in coordinae geografiche (latitudine e longitudine) */
+
+
+ 
+d3.select('#graph > svg')
+.append('g')
+.selectAll('path')
+.data(geoJSON)
+.enter()
+.append('path')
+.attr('d', path) 
+.attr('fill', '#ccc')
+.attr('stroke','white')
+.attr('class','county')
